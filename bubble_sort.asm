e77f0e3592a16b9505f123a7185fd59c2edcc69d
Charles Summers <pl.charlessummers@gmail.com>
2020-10-09 18:13:27 +0200

    Tab changes

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -3,20 +3,20 @@ ARR: 	DS 7					; Space for array declared here.
 
 CSEG AT 0						; Code segment - program's code memory (read only!).
 RESET:
-	MOV SP, #7FH				; Stack pointer filled with value 7F. 8032/8052 (8051?) architecture assumed.
+	MOV 	SP, #7FH			; Stack pointer filled with value 7F. 8032/8052 (8051?) architecture assumed.
 
 COPY_ARR:
-	MOV DPTR, #ARR_DATA			; DPTR points to values in our array in code memory.
-	MOV R0, #ARR				; R0 points to the place reserved for the array in internal memory.
-	MOV R7, #7					; Array size moved to R7 for loop counting purpose.
+	MOV 	DPTR, #ARR_DATA		; DPTR points to values in our array in code memory.
+	MOV		R0, #ARR			; R0 points to the place reserved for the array in internal memory.
+	MOV		R7, #7				; Array size moved to R7 for loop counting purpose.
  
 COPY_LP:
-	CLR A						; Accumulator is cleared.
-	MOVC A, @A + DPTR			; Consecutive elements from array are stored in accumulator.
-	MOV @R0, A					; The element is moved from the accumulator to the place reserved for them in data segment.
-	INC DPTR					; DPTR points to the next element.
-	INC R0						; R0 points to the next available space.
-	DJNZ R7, COPY_LP			; The loop is repeated arr.size() times.
+	CLR 	A					; Accumulator is cleared.
+	MOVC 	A, @A + DPTR		; Consecutive elements from array are stored in accumulator.
+	MOV 	@R0, A				; The element is moved from the accumulator to the place reserved for them in data segment.
+	INC 	DPTR				; DPTR points to the next element.
+	INC 	R0					; R0 points to the next available space.
+	DJNZ 	R7, COPY_LP			; The loop is repeated arr.size() times.
 
 	MOV		R6, #7				; Array size stored in R6 (outer loop condition)
 
@@ -53,7 +53,7 @@ COMPARE:
 	DEC		R0					; R0 <- *arr[i]
 
 CORR:
-	INC 	R0					; R0 holds the address of the next element.
+	INC		R0					; R0 holds the address of the next element.
 	DJNZ	R7, COMPARE			; Iterate over the array up to second to last element (we select pairs).
 	
 	DJNZ	R6, SORT			; Repeat the process arr.size() times.
